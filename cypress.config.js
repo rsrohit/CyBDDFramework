41c4a5749db78ce327cba2d83dd17f16d655be83
Rohit Shinde <86065014+rohShinde@users.noreply.github.com>
2023-04-20 22:41:19 +0530

    github action and cypress cloud code added

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
+    projectId: "nvh163",
     e2e: {
         baseUrl: "https://pro.missiongraph.io/",
         setupNodeEvents(on, config) {
